b832b09b5e5bfa27d0b9d1b3287edeeaccd32a7d
Randy Clark <randy.clark@live.com>
2020-10-07 23:33:05 -0400

    Delete QualificationViewModel.cs

--- Resume/ViewModels/QualificationViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Resume.ViewModels
-{
-    public class QualificationViewModel
-    {
-        public int TechnicalSkillTypeId { get; set; }
-        public string TechnicalSkillTypeDescription { get; set; }
-        public string TechnicalSkillDescription { get; set; }
-    }
-}
