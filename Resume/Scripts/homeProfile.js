4eca23c32bd76c15f2944e73d26d62bf65c38dc6
Randy M Clark <randy.clark@live.com>
2020-11-01 18:56:11 -0500

    Added spread operators and map functions in place of traditional for() loops

--- Resume/Scripts/homeProfile.js
+++ Resume/Scripts/homeProfile.js
@@ -2,20 +2,19 @@
     window.TERMINAL = {
         /**
          * @description - Emulate blinking terminal cursor
-         * @param {HTMLParagraphElement} welcome
-         * @param {string} welcomeString
-         * @param {any} controller
+         * @param {HTMLParagraphElement}
+         * @param {string}
          * @returns {null}
          */
-        hello: function (welcome, welcomeString) {
-            let letters = welcomeString.split("");
+        typeMessage: (el, welcomeString) => {
+            letters = welcomeString.split("");
             if (letters.length > 0) {
                 let intervalId = setInterval(function () {
                     if (letters.length > 0) {
                         let lastLetter = letters.shift();
                         let spanChar = document.createElement("SPAN");
                         //  set all individual <span>text letters</span> same bg & color
-                        [].slice.call(welcome.children).forEach(child => {
+                        [].slice.call(el.children).forEach(child => {
                             with (child.style) {
                                 color = "#000";
                                 backgroundColor = "#00ffb4";
@@ -27,7 +26,7 @@
                             backgroundColor = "#000";
                             color = "#00ffb4";
                         };
-                        welcome.appendChild(spanChar);
+                        el.appendChild(spanChar);
                     } else {
                         window.clearInterval(intervalId);
                         //  mimic keyboard cursor flashing as if ready for input
@@ -45,50 +44,50 @@
         },
         /**
          * @description - Removes the final '|' char in html string row
-         * @param {HTMLSpanElement} lastSpan
+         * @param {HTMLSpanElement}
          * @returns {null}
          */
-        removepipe: function (lastSpan) {
+        removeLastPipe: lastSpan => {
             lastSpan ? lastSpan.remove() : null;
         },
         /**
          * @description - Sets addEventListeners for any dom element children as function parameter
-         * @param {CSSRule} paragraphTitles
+         * @param {CSSRule}
          * @returns {null}
          */
-        zoomsections: function (paragraphTitles) {
-            for (let p = 0; p < paragraphTitles.length; p++) {
-                if (!paragraphTitles[p].classList.contains("noevent")) {
-                    paragraphTitles[p].addEventListener("click", function (e) {
+        zoomsections: paragraphTitles => {
+            [...paragraphTitles].map(p => {
+                if (!p.classList.contains("noevent")) {
+                    p.addEventListener("click", function (e) {
                         let title = TARGET.getTarget(e);//<span>
                         let grandParent = title.parentNode.parentNode;// represents grandparnet <td> container of previous <span>
-
 
                         let paragraphs = grandParent.querySelectorAll("div:not(:first-of-type) p");
                         if (title.textContent.toLowerCase() == "architecture") {
                             paragraphs = grandParent.querySelectorAll("div:not(:first-of-type) table")
                         };
-                        for (let j = 0; j < paragraphs.length; j++) {
-                            paragraphs[j].classList.toggle("zoom-text");
-                        };
+                        [...paragraphs].map(p => {
+                            p.classList.toggle("zoom-text");
+                        });
                     });
                 };
-            };
+            });
         },
         /**
          * @description - Sets event listeners on tables to be displayed
-         * @param {HTMLSpanElement} spantags
+         * @param {HTMLSpanElement}
          * @returns {null}
          */
-        displayTables: function (spantags) {
-            for (s = 0; s < spantags.length; s++) {
-                let span = spantags[s];
+        displayTables: els => {
+            [...els].map(el => {
+                let span = el;
                 span.addEventListener("click", () => {
+                    span.classList.toggle("text-Highlight");
                     let table = span.parentNode.parentNode.parentNode.parentNode;
                     let tbody = table.children[1];//[1] = <tbody>  while [0] = <thead>
                     tbody.classList.toggle("display-table");
                 });
-            }
+            });
         }
     }
 })(window);
